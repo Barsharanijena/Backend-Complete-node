b50a3c5e204f789291a447546ded7d7dc7676cf9
Barsharani Jena <Barsharanijena555@gmail.com>
2025-09-07 16:33:29 +0530

    new code

--- 4.path-module/index.js
+++ 4.path-module/index.js
@@ -8,3 +8,12 @@ console.log('file extension', path.extname(__filename));
 
 const joinpath = path.join("/user" ,"document","node","projects");
 console.log("joined path", joinpath);
+
+// Normalizing a path
+const abnormalPath = "/user//document/../document/node//projects";
+const normalPath = path.normalize(abnormalPath);
+console.log("Normalized path:", normalPath);
+
+// Resolving absolute path
+const resolvePath = path.resolve("user", "document", "node", "projects");
+console.log("Resolved absolute path:", resolvePath);
