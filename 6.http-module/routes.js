001ab4dae6cd52e9d9cde8cb988feb4c06800a20
Barsharani Jena <Barsharanijena555@gmail.com>
2025-09-09 20:00:53 +0530

    new module

--- 6.http-module/routes.js
+++ 6.http-module/routes.js
@@ -1,7 +1,20 @@
 const http = require ('http');
-const server = http.createServer((req,res)=>{
 
+const server = http.createServer((req,res)=>{
+    const url = req.url;
+    if(url === '/'){
+       res.writeHead(200,{"content-type":"text/plain"});
+       res.end("Home page")
+    } else if(url === '/project'){
+       res.writeHead(200,{"content-type":"text/plain"});
+       res.end("Projects");
+    } else{
+        res.writeHead(404,{"content-type":"text/plain"});
+       res.end("This page can not be found!");
+    }
 });
 
 const port = 3000;
-server.listen(port)
+server.listen(port, ()=>{
+    console.log(`Server is now listening to port ${port}`);
+})
