413534cfa95f70f06aaaa148fc30ca1f3867a507
Barsharani Jena <Barsharanijena555@gmail.com>
2025-09-13 19:38:57 +0530

    new module

--- 8.promises/index.js
+++ 8.promises/index.js
@@ -8,4 +8,16 @@ delayFn(2000).then(()=> console.log('after 2 seconds promise resolved'));
 console.log("end");
 
 
-function divide
+function divideFn(num1, num2){
+    return new Promise((resolve, reject)=>{
+        if(num2 === 0){
+            reject('Can not perform division by 0')
+        } else {
+            resolve(num1/num2)
+        }
+    })
+}
+
+divideFn(10,5)
+.then(result => console.log(result, 'result'))
+.catch(error => console.log(error,"error"));
