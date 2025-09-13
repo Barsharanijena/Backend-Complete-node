78ace8424ed9049e7d9982df950fd94e4af07457
Barsharani Jena <Barsharanijena555@gmail.com>
2025-09-13 19:55:27 +0530

    new module

---
+++ 9.async-await/index.js
@@ -0,0 +1,33 @@
+const { error } = require("node:console");
+
+function delayFn(time){
+    return new Promise((resolve)=> setTimeout(resolve, time))
+}
+
+async function delayedGreet(name) {
+    await delayFn(2000)
+    console.log(name)
+    
+}
+
+delayedGreet("Barsha");
+
+
+async function division(num1,num2) {
+    try{
+        if(num2 === 0) throw new Error("can not divide by 0");
+        return num1/ num2;
+
+    }catch(error){
+        console.error('error', error)
+        return null
+    }
+    
+};
+
+async function mainFn(){
+    console.log(await division(10,2))
+    console.log(await division(10,0))
+}
+
+mainFn();
